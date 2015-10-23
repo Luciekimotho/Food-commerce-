b2405f358a1548e7624cfba8276c3feac41af9de
Luciekimotho <luciekimotho@gmail.com>
2015-10-23 15:06:44 +0300

    database settings changed

--- chapchap/settings.py
+++ chapchap/settings.py
@@ -144,4 +144,12 @@ OSCAR_ORDER_STATUS_PIPELINE = {
 }
 
 
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
