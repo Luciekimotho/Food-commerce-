9286ba325d527467ea62c54e769b834a96b8cd27
Luciekimotho <luciekimotho@gmail.com>
2015-10-23 16:06:54 +0300

    Correcting error in the wsgi file

--- chapchap/wsgi.py
+++ chapchap/wsgi.py
@@ -15,7 +15,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chapchap.settings")
 
 application = get_wsgi_application()
 try:
-    from dj import Cling
+    from dj_static import Cling
     application = Cling(get_wsgi_application())
 except:
     pass
